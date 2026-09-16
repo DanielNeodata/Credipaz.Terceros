3c53543244d6069e8ed3ec0092ef308571b412b9
Daniel Fernandez <daniel@gruponeodata.com>
2026-09-16 09:20:45 -0300

    neodata

--- js/API.js
+++ js/API.js
@@ -161,7 +161,7 @@ var _API = {
                 /*Resuelve la navegación y display de la url de Interfaces */
                 if (!_url.includes("?")) { _url += "?"; } else { _url += "&"; }
                 _url += ("id_user_active=" + _API.id_user_log + "&username=" + _API.username_log + "&id_sucursal=" + _API.id_sucursal + "&sucursal=" + _API.sucursal);
-                var _html = "<iframe id='neoweb_iframe' class='neoweb_iframe' src='" + encodeURIComponent(_url) + "' frameborder='0' style='height:500vh;width:100%;'></iframe>";
+                var _html = "<iframe id='neoweb_iframe' class='neoweb_iframe' src='" + encodeURI(_url) + "' frameborder='0' style='height:500vh;width:100%;'></iframe>";
                 $(".areaResultado").html(_html).removeClass("d-none");
                 setTimeout(function () { _API.onWait(false); }, 500);
                 break;
@@ -358,8 +358,6 @@ var _API = {
             $(_target).html(response.html).removeClass("d-none");
             /*Setea el color del menú de acuerdo a la configuración de la rama*/
             if (_API.branchConfiguration.menuColor != "") { $(".itemMenu").addClass(_API.branchConfiguration.menuColor); }
-            /*Remueve items del menú que aún apuntan al esquema PHP de acceso a models y controllers*/
-            $(".deprecated").remove();
         });
     },
     onSucursalChooser: function (_auth) {
