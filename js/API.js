46f86768042a739ad2b6a487b9b5b3a40210252b
Daniel Fernandez <daniel@gruponeodata.com>
2026-09-24 11:39:39 -0300

    neodata

--- js/API.js
+++ js/API.js
@@ -209,7 +209,7 @@ var _API = {
                 /*Resuelve la navegación y display de la url de Interfaces */
                 if (!_url.includes("?")) { _url += "?"; } else { _url += "&"; }
                 _url += ("id_user_active=" + _API.id_user_log + "&username=" + _API.username_log + "&id_sucursal=" + _API.id_sucursal + "&sucursal=" + _API.sucursal);
-                var _html = "<iframe id='neoweb_iframe' class='neoweb_iframe' src='" + encodeURI(_url) + "' frameborder='0' style='height:500vh;width:100%;'></iframe>";
+                var _html = "<iframe id='neoweb_iframe' class='neoweb_iframe' src='" + encodeURI(_url) + "' allow='display-capture; autoplay; camera; microphone' frameborder='0' style='height:500vh;width:100%;'></iframe>";
                 setTimeout(function () { _API.onLoadAreaResultado(_html); }, 1000);
                 break;
             default:
